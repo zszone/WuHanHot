5e10f96c3901037ac7fe60699bbd6eb03d681a3d
zs <zszone@zszones-macbook-pro.local>
2016-05-18 20:07:16 +0800

    删除无用代码

---
+++ WuHanHot/WuHanHot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WuHanHot
+//
+//  Created by zs on 16/5/18.
+//  Copyright © 2016年 zs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WuHanHot/WuHanHot/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  WuHanHot
+//
+//  Created by zs on 16/5/18.
+//  Copyright © 2016年 zs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"!23");
+}
+
+@end
